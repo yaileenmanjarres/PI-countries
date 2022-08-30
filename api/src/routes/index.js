699661f0833343816787bb61885e170a0dfe6ea5
Yaileen Manjarres <yaileenmanjarres@gmail.com>
2022-08-30 14:04:26 -0500

    Back listo

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,7 @@
-const { Router } = require('express');
+const { Router } = require('express')
 const { conn } = require('../db')
-const { Country, Activity } = conn.models;
+const { Op } = require("sequelize")
+const { Country, Activity } = conn.models
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -12,17 +13,37 @@ const router = Router();
 
 router.get('/countries', async (req, res) => {
   const { name } = req.query
+  const { page } = req.body
+  const offset = page ? (page === 1 ? 0 : 10 * page) : 0
   if (name) {
-    res.json(name)
+    const nameCountry = await Country.findAll({
+      limit: 10,
+      offset,
+      where: {
+        name: { [Op.like]: `%${name.replace(/^\w/, (letter) => letter.toUpperCase())}%` }
+      }
+    })
+    res.json(nameCountry)
   } else {
-    const allCountries = await Country.findAll()
+    const allCountries = await Country.findAll({
+      limit: 10,
+      offset,
+      attributes: ['id', 'name', 'flag', 'region', 'capital']
+    })
     res.json(allCountries)
   }
 })
 
-router.get('/countries/:idPais', (req, res) => {
-  console.log(req.params);
-  res.json(req.params)
+router.get('/countries/:id', async (req, res) => {
+  const { id } = req.params
+  if (id) {
+    const countryId = await Country.findOne({
+      where: {
+        id: id.toUpperCase()
+      }
+    })
+    res.json(countryId)
+  }
 })
 
 router.post('/activities', (req, res) => {
